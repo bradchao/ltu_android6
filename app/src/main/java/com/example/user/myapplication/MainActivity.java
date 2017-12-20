296799437298d4cc92aac6c20d981437bdb6168b
bradchao <brad@brad.tw>
2017-12-20 13:48:21 +0800

    v3

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.user.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webVidew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webVidew = (WebView)findViewById(R.id.webView);
+        initWebView();
+    }
+
+    private void initWebView(){
+        webVidew.setWebViewClient(new WebViewClient());
+
+        WebSettings settings = webVidew.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        webVidew.loadUrl("https://tw.yahoo.com/");
     }
 }
